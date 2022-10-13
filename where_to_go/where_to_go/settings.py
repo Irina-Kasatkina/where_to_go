7e3bbefe1a1653b5909b764f94ecc17e6e59b982
Irina Kasatkina <kasatkina235@gmail.com>
2022-10-13 16:04:16 +0300

    Setup media files serving

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -21,7 +21,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -32,7 +31,6 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'REPLACE_ME')
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -130,7 +128,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
+print(MEDIA_ROOT)
 MEDIA_URL = '/media/'
 
 # Default primary key field type
