c1d764d8b41a01926daead74327efe1e0aa7edf0
Irina Kasatkina <kasatkina235@gmail.com>
2022-10-13 08:02:44 +0300

    Create places app

--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -1,3 +1,4 @@
+# from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
@@ -7,3 +8,4 @@ def show_places_page(request):
     context = {}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
+# Create your views here.
