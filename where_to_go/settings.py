81c11b51284fb6512c714286a3a71d61adf9caf5
Irina Kasatkina <kasatkina235@gmail.com>
2022-10-15 10:29:54 +0300

    Change settings.py for pythonanywhere.com

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -129,9 +129,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
