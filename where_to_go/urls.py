1f10b1e9652c7e251aa5c9b183028f501c1dd831
Irina Kasatkina <kasatkina235@gmail.com>
2022-10-17 18:39:55 +0300

    Fix broken image previews in admin on pythonanywhere.com

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve as mediaserve
 
 from places import views
 
@@ -27,4 +28,13 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+                mediaserve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+                mediaserve, {'document_root': settings.STATIC_ROOT}),
+    ]
